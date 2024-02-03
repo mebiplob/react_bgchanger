97d92f10c723a4aec5613d06cf3e572b1147f849
mebiplob <syedafnanfreelancer@gmail.com>
2024-02-03 20:55:12 +0600

    React BG Changer

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 
 function App() {
+  const [color, setColor] = useState("olive")
   return (
     <>
-      <h1>Background Changer</h1>
+      <div className='h-screen w-full' style={{backgroundColor: color}}></div>
+     
+      <div className='flex fixed bottom-12 shadow-md justify-center items-center gap-5 py-8 px-10'>
+          <button onClick={() => { setColor("white")}} className='btn bg-white text-black rounded-full'>White</button>
+          <button onClick={() => { setColor("black")}} className='btn bg-black text-white rounded-full'>Black</button>
+          <button onClick={() => { setColor("blue")}} className='btn bg-blue-400 text-black rounded-full'>Blue</button>
+          <button onClick={() => { setColor("green")}} className='btn bg-green-400 text-black rounded-full'>Green</button>
+          <button onClick={() => { setColor("yellow")}} className='btn bg-yellow-400 text-black rounded-full'>Yellow</button>
+      </div>
+
     </>
   )
 }
