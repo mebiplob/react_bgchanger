e183f55b418de11909ba02aadac7fa8306645635
mebiplob <syedafnanfreelancer@gmail.com>
2024-02-03 21:02:10 +0600

    React BG Changer

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -4,14 +4,14 @@ function App() {
   const [color, setColor] = useState("olive")
   return (
     <>
-      <div className='h-screen w-full' style={{backgroundColor: color}}></div>
+      <div className='h-screen w-full duration-200' style={{backgroundColor: color}}></div>
      
-      <div className='flex fixed bottom-12 shadow-md justify-center items-center gap-5 py-8 px-10'>
-          <button onClick={() => { setColor("white")}} className='btn bg-white text-black rounded-full'>White</button>
-          <button onClick={() => { setColor("black")}} className='btn bg-black text-white rounded-full'>Black</button>
-          <button onClick={() => { setColor("blue")}} className='btn bg-blue-400 text-black rounded-full'>Blue</button>
-          <button onClick={() => { setColor("green")}} className='btn bg-green-400 text-black rounded-full'>Green</button>
-          <button onClick={() => { setColor("yellow")}} className='btn bg-yellow-400 text-black rounded-full'>Yellow</button>
+      <div className='flex fixed bottom-12 shadow-md justify-center items-center gap-5 py-8 px-10 rounded-full'>
+          <button onClick={() => { setColor("white")}} className='btn bg-white text-black rounded-full px-3 py-1'>White</button>
+          <button onClick={() => { setColor("black")}} className='btn bg-black text-white rounded-full px-3 py-1'>Black</button>
+          <button onClick={() => { setColor("blue")}} className='btn bg-blue-400 text-black rounded-full px-3 py-1'>Blue</button>
+          <button onClick={() => { setColor("green")}} className='btn bg-green-400 text-black rounded-full px-3 py-1'>Green</button>
+          <button onClick={() => { setColor("yellow")}} className='btn bg-yellow-400 text-black rounded-full px-3 py-1'>Yellow</button>
       </div>
 
     </>
